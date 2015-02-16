abf8064f9b790e695658f97975546732e739c1f1
Dolmake <demondolmake@hotmail.com>
2015-02-16 13:05:39 +0100

    Model created

---
+++ SoccerZero/SoccerZero/DLMKBaseManagedObject.h
@@ -0,0 +1,13 @@
+//
+//  DLMKBaseManagedObject.h
+//  SoccerZero
+//
+//  Created by Daniel on 16/02/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface DLMKBaseManagedObject : NSManagedObject
+
+@end
---
+++ SoccerZero/SoccerZero/DLMKBaseManagedObject.m
@@ -0,0 +1,13 @@
+//
+//  DLMKBaseManagedObject.m
+//  SoccerZero
+//
+//  Created by Daniel on 16/02/15.
+//  Copyright (c) 2015 Dolmake. All rights reserved.
+//
+
+#import "DLMKBaseManagedObject.h"
+
+@implementation DLMKBaseManagedObject
+
+@end
